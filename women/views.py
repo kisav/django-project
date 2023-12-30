3841682fb7789a4270cc16aca588daf6f94c3855
vasik <4vlad.ilyin@gmail.com>
2023-12-30 21:47:15 +0300

    first commmit

---
+++ women/views.py
@@ -0,0 +1,46 @@
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+def index(request): # HttpRequest
+    data = {
+            'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 2, 3, 4, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponseRedirect(uri)
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
