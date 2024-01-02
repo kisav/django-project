3515a43ff555632b601b30a4a6b47e6ee6fdbd9d
vasik <4vlad.ilyin@gmail.com>
2024-01-02 17:06:39 +0300

    add menu

--- women/views.py
+++ women/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -12,33 +17,35 @@ data_db = [
 ]
 
 
-def index(request): # HttpRequest
+def index(request):
     data = {
-            'title': 'главная страница',
-            'menu': menu,
-            'posts': data_db,
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
     }
     return render(request, 'women/index.html', context=data)
+
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        return HttpResponseRedirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
