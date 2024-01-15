c37a78ee97c6899af69dd80a4a14cdd341e2e91d
vasik <4vlad.ilyin@gmail.com>
2024-01-15 11:01:38 +0300

    add static

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ywt^i-jce1cq%5q4s^p0130-h9x9vdmvmge-vf8g+htkga+=71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
